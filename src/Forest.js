ddaa990742ccc5ba132a38dbbd1aca4a6a2e92d3
Mathieu Livebardon <mathieu.livebardon@universite-lyon.fr>
2024-04-13 12:35:46 +0200

    feat(Forest):class ModelController

---
+++ src/Forest.js
@@ -0,0 +1,77 @@
+/**
+ * @typedef Object Cell
+ * @property {string} state
+ * @property {{top:{number:x,num:y}||null,bottom:{number:x,num:y}||null,right:{number:x,num:y}||null,left:{number:x,num:y}}} neighbours
+ */
+
+export class Forest {
+  constructor(h, l, probProp, positionsOnFire) {
+    /**@type {number} */
+    this.h = h;
+    this.l = l;
+    this.probProp = probProp;
+    this.positionsOnFire = positionsOnFire;
+    /**@type {Array.<Cell[]>} */
+    this.grid = [];
+    this.initGrid();
+  }
+
+  initGrid() {
+    for (let y = 0; y < this.h; y++) {
+      this.grid.push([]);
+      for (let x = 0; x < this.l; x++) {
+        const neighbours = {
+          top: y + 1 < this.h ? { x, y: y + 1 } : null,
+          bottom: y - 1 >= 0 ? { x, y: y - 1 } : null,
+          right: x + 1 < this.l ? { x: x + 1, y } : null,
+          left: x - 1 >= 0 ? { x: x - 1, y } : null,
+        };
+        const newCell = {
+          state: State.NEUTRAL,
+          neighbours: neighbours,
+          pos: { x, y },
+        };
+        this.grid[y].push(newCell);
+      }
+    }
+
+    this.positionsOnFire.forEach((pos) => {
+      this.grid[pos.y][pos.x].state = State.ON_FIRE;
+    });
+  }
+
+  nextStep() {
+    const cellsOnFire = [];
+    this.grid.forEach((line) => {
+      line.forEach((cell) => {
+        if (cell.state == State.ON_FIRE) cellsOnFire.push(cell);
+      });
+    });
+
+    if (!cellsOnFire.length) {
+      console.log("Fin de la propagation");
+      return;
+    }
+
+    cellsOnFire.forEach((cell) => {
+      cell.state = State.BURNED;
+      Object.keys(cell.neighbours).forEach((key) => {
+        const n = cell.neighbours[key];
+        if (!n) return;
+
+        if (this.grid[n.y][n.x].state == State.NEUTRAL) {
+          const rand = Math.random();
+          if (rand < this.probProp) {
+            this.grid[n.y][n.x].state = State.ON_FIRE;
+          }
+        }
+      });
+    });
+  }
+}
+
+const State = {
+  NEUTRAL: "neutral",
+  ON_FIRE: "on_fire",
+  BURNED: "burned",
+};
